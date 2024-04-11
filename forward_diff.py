8bea5a04e01a1c8a68be7f053973a57fbe6eec90
Thomaswang0822 <thomaswang0822@outlook.com>
2024-04-10 21:15:55 -0700

    passed all intrinsic computation tests

--- forward_diff.py
+++ forward_diff.py
@@ -178,9 +178,117 @@ def forward_diff(diff_func_id : str,
                 [val, dval]
             )
 
+        """handle intrinsic function calls (sin, exp, etc.)
+        and let others pass by.
+        """
         def mutate_call(self, node):
-            # HW1: TODO
-            return super().mutate_call(node)
+            # these intrinsic functions have at least 1 arg, 
+            # (pow has 2) we call it x (and y)
+            if len(node.args) == 0:
+                return super().mutate_call(node)
+            x = self.mutate_expr(node.args[0])
+            x_val, x_dval = loma_ir.StructAccess(x, 'val'), loma_ir.StructAccess(x, 'dval')
+            # for returned d_float
+            val = loma_ir.Call(node.id, [x_val]) if node.id != "pow" else None
+            dval = None  # to be filled in swtich
+            match node.id:
+                case "sin":
+                    dval = loma_ir.BinaryOp(
+                        loma_ir.Mul(), 
+                        loma_ir.Call("cos", [x_val]), 
+                        x_dval
+                    )  #  cos(x.val) * x.dval
+                case "cos":
+                    sinx_dx = loma_ir.BinaryOp(
+                        loma_ir.Mul(), 
+                        loma_ir.Call("sin", [x_val]), 
+                        x_dval
+                    )  #  sin(x.val) * x.dval
+                    #  0 - sin(x.val) * x.dval
+                    dval = loma_ir.BinaryOp(loma_ir.Sub(), loma_ir.ConstFloat(0.0), sinx_dx)
+                case "sqrt":
+                    dval = loma_ir.BinaryOp(
+                        loma_ir.Mul(),
+                        loma_ir.BinaryOp(
+                            loma_ir.Div(),
+                            loma_ir.ConstFloat(0.5), 
+                            loma_ir.Call("sqrt", [x_val])
+                        ),
+                        x_dval
+                    )  # (0.5 / sqrt(x)) * x_dval
+                case "pow":
+                    assert len(node.args) == 2, "pow() should have 2 args"
+                    y = self.mutate_expr(node.args[1])
+                    y_val = loma_ir.StructAccess(y, 'val')
+                    y_dval = loma_ir.StructAccess(y, 'dval')
+                    # d/dx(x^y) = y * x^(y-1)
+                    # d/dy(x^y) = x^y * log(x)
+                    partial_x = loma_ir.BinaryOp(
+                        loma_ir.Mul(),
+                        y_val,
+                        loma_ir.Call(
+                            "pow", [
+                                x_val,
+                                loma_ir.BinaryOp(
+                                    loma_ir.Sub(),
+                                    y_val,
+                                    loma_ir.ConstFloat(1.0), 
+                                )  # y-1
+                            ]
+                        )
+                    )  # y * x^(y-1)
+                    partial_y = loma_ir.BinaryOp(
+                        loma_ir.Mul(),
+                        loma_ir.Call(
+                            "log",
+                            [x_val]
+                        ),  # log(x)
+                        loma_ir.Call(
+                            "pow", [
+                                x_val,
+                                y_val
+                            ]
+                        ) # x^y
+                    )  # log(x) * x^y
+                    dval = loma_ir.BinaryOp(
+                        loma_ir.Add(),
+                        loma_ir.BinaryOp(
+                            loma_ir.Mul(),
+                            x_dval, 
+                            partial_x
+                        ),
+                        loma_ir.BinaryOp(
+                            loma_ir.Mul(),
+                            y_dval, 
+                            partial_y
+                        ),
+                    )  # partial_x * dx + partial_y * dy
+                    # val is also different since we need one more arg
+                    val = loma_ir.Call(node.id, [x_val, y_val])
+                case "exp":
+                    dval = loma_ir.BinaryOp(
+                        loma_ir.Mul(), 
+                        loma_ir.Call("exp", [x_val]), 
+                        x_dval
+                    )  #  exp(x.val) * x.dval
+                case "log":
+                    dval = loma_ir.BinaryOp(
+                        loma_ir.Mul(),
+                        loma_ir.BinaryOp(
+                            loma_ir.Div(),
+                            loma_ir.ConstFloat(1.0), 
+                            x_val
+                        ),
+                        x_dval
+                    )  # (1 / x) * x_dval
+                case _:
+                    # non-intrinsic function with >=0 args
+                    return super().mutate_call(node)
+            # return for the intrinsic
+            return loma_ir.Call(
+                'make__dfloat', 
+                [val, dval]
+            )
 
         """helper function that extract the val and dval for
         the 2 operands in a binary op
