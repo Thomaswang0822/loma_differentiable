30773857988d39e02e707b64d495859e8df478c4
Thomaswang0822 <thomaswang0822@outlook.com>
2024-06-01 13:13:16 -0700

    let Teg compute the correct results

--- param_dis_examples/reparam_host.py
+++ param_dis_examples/reparam_host.py
@@ -6,29 +6,31 @@ sys.path.append(parent)
 import compiler
 import ctypes
 from numpy.random import rand
+# Teg import
+from teg import TegVar, Var, Teg, IfElse
+from teg.derivs import FwdDeriv
+from teg.eval.numpy_eval import evaluate
 
 
 #===========Check loma_code/reparam.py and define here===========
-_m, _k = -3.0, 0.5
+_m, _k = 3.0, 0.5
 _n, _p = 0.0, 0.0
-_op = ">"
+_op = "<"
 #===========Check loma_code/reparam.py and define here===========
 
-"""
+"""Manual log
 3x < 0.5t -> positive; CORRECT
 3x > 0.5t -> negative; CORRECT
 
--3x < 0.5t -> positive; (correct integral, 0, wrong expect dval)
--3x > 0.5t -> negative; (correct integral, 0, wrong expect dval)
+-3x < 0.5t -> positive; CORRECT
+-3x > 0.5t -> negative; CORRECT
 
 3x < -0.5t -> negative; CORRECT
 3x > -0.5t -> positive; CORRECT
 
--3x < -0.5t -> negative; (correct integral, 0, wrong expect dval)
--3x > -0.5t -> positive; (correct integral, 0, wrong expect dval)
+-3x < -0.5t -> negative; CORRECT
+-3x > -0.5t -> positive; CORRECT
 """
-positive = bool("<" in _op) == bool(_k > 0.0)
-interval_reversed = False
 
 def t_should_between(low, up):
     """This time the integrand has general form
@@ -39,7 +41,7 @@ def t_should_between(low, up):
     between low and up.
 
     In the compiler implementation, we check
-    [R(lower) < 0 < R(upper)]
+    [_m*R(lower) < 0 < _m*R(upper)]
     Here, to test, we do a different manual computation.
     And t should produce non-zero derivative when
     [lhs < t < rhs]
@@ -47,35 +49,30 @@ def t_should_between(low, up):
     # now, t must be between
     lhs = (_m * low + _n - _p) / _k
     rhs = (_m * up + _n - _p) / _k
-    if lhs < rhs:
-        global interval_reversed
-        interval_reversed = True
+    # ensure lhs < rhs
+    if lhs > rhs:
         lhs, rhs = rhs, lhs
     return lhs, rhs
 
-def correct_dval() -> float:
-    return _k/_m * (1.0 if "<" in _op else -1.0)
+# Let Teg compute the correct val and dval for reference
+def correct_val_dval(_t: float):
+    x, t = TegVar('x'), Var('t', _t)
 
-def correct_val(lhs, rhs, interval_length, t) -> float:
-    numerator = (t-lhs) if positive else (rhs-t)
-    return numerator / (rhs - lhs) * interval_length
+    # we can't introduce IfElse (discountinuity) to this function,
+    # so we define it dynamically
+    if _op in ("<", "<="):
+        def integrand_pd():
+            return (_m * x + _n) < (_k * t + _p)
+    elif _op in (">", ">="):
+        def integrand_pd():
+            return (_m * x + _n) > (_k * t + _p)
+    else:
+        assert False, "TYPO"
 
-def correct_vals_dvals(choices, lhs, rhs, interval_length):
-    vals, dvals = [], []
-    for t in choices:
-        if t <= lhs:
-            # first and last, out of bound with zero derivative
-            dvals.append(0.0)
-            vals.append(interval_length * (1 - int(positive)))
-        elif t >= rhs:
-            dvals.append(0.0)
-            vals.append(interval_length * int(positive))
-        else:
-            # non-zero case
-            dvals.append(correct_dval())
-            vals.append(correct_val(lhs, rhs, interval_length, t))
-
-    return vals, dvals
+    body = IfElse(integrand_pd(), 1, 0)
+    expr = Teg(5, 7, body, x)
+    deriv_expr = FwdDeriv(expr, [(t, 1)])
+    return evaluate(expr), evaluate(deriv_expr)
 
 if __name__ == '__main__':
     with open('loma_code/reparam.py') as f:
@@ -89,17 +86,17 @@ if __name__ == '__main__':
     _dfloat = structs['_dfloat']
     dlow, dup = _dfloat(low, 0.0), _dfloat(up, 0.0)
 
-    rhs, lhs = t_should_between(low, up)
+    lhs, rhs = t_should_between(low, up)
     # choices = [lhs-1.0, lhs + (rhs - lhs) * float(rand()), rhs+1.0]
     choices = [lhs-1.0, 34.0 * (1 if _k * _m > 0 else -1), rhs+1.0]
-    vals, dvals = correct_vals_dvals(choices, lhs, rhs, up-low)
 
     print(f"We take integral from low={low} to up={up}")
     print(f"integrand is indicator [{_m}*x+{_n} {_op} {_k}*t+{_p}]")
     print(f"And t should produce non-zero integral value and derivative when [{lhs} < t < {rhs}]")
 
-    for t, val, dval in zip(choices, vals, dvals):
-        df_t = _dfloat(t, 1.0)
+    for _t in choices:
+        val, dval = correct_val_dval(_t)
+        df_t = _dfloat(_t, 1.0)
         res = lib.fwd_IntegralEval(dlow, dup, df_t)
-        print(f"With t={t:.3f}, Integral is {res.val:.3f}, expect {val:.3f} "
+        print(f"With t={_t:.3f}, Integral is {res.val:.3f}, expect {val:.3f} "
             f"derivative is {res.dval:.3f}, expect {dval:.3f}")
