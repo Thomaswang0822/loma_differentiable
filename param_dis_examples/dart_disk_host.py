90697c73d72bb98a79e7443eac88cf7e892734ca
thomaswang0822 <thomaswang0822@outlook.com>
2024-06-12 21:35:34 -0700

    set the skeleton of application

---
+++ param_dis_examples/dart_disk_host.py
@@ -0,0 +1,22 @@
+import os
+import sys
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+import compiler
+import ctypes
+from numpy.random import rand
+# Teg import
+from teg import TegVar, Var, Teg, IfElse
+from teg.derivs import FwdDeriv
+from teg.eval.numpy_eval import evaluate
+
+
+if __name__ == '__main__':
+    with open('loma_code/dart_disk.py') as f:
+        structs, lib = compiler.compile(
+            f.read(),
+            target = 'c',
+            output_filename = '_code/dart_disk'
+        )
+    print("********COMPILED SUCCESSFULLY********")
